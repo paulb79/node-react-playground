c8b29277d0a3fa1e0dc6ad363feb936e1aab97b4
Paul Brown <2188950+paulb79@users.noreply.github.com>
2020-09-24 23:14:32 +0100

    Improved the nav to include a get weather form

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -1,40 +1,47 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = (props) => {
-    return (
-        <div className="top-bar">
-            <div className="top-bar-left">
-                <ul className="menu">
-                    <li className="menu-text">
-                        Playground App
-                    </li>
-                    <li>
-                        <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
-                    </li>
-                    <li>
-                        <Link to="/about" activeClassName="active">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/examples" activeClassName="active">Examples</Link>
-                    </li>
-                    <li>
-                        <Link to="/countdown" activeClassName="active">Countdown</Link>
-                    </li>
-                    <li>
-                        <Link to="/timer" activeClassName="active">Timer</Link>
-                    </li>
-                </ul>
+var Nav = React.createClass({
+    onSearch: function (e) {
+        e.preventDefault();
+        alert("Todo ")
+    },
+    render: function () {
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">
+                            Playground App
+                        </li>
+                        <li>
+                            <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/about" activeClassName="active">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/examples" activeClassName="active">Examples</Link>
+                        </li>
+                        <li>
+                            <Link to="/countdown" activeClassName="active">Countdown</Link>
+                        </li>
+                        <li>
+                            <Link to="/timer" activeClassName="active">Timer</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSeach}>
+                        <ul className="menu">
+                            <li><input type="search" placeholder="Enter a city name" /></li>
+                            <li><button type="button" className="button">Get Weather</button> </li>
+                        </ul>
+                    </form>
+                </div>
             </div>
-            <div className="top-bar-right">
-                <ul className="menu">
-                    <li className="menu-text">
-                        <a href="https://redjamjar.net" target="_blank">Paul Brown</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+        )
+    }
+});
 
 module.exports = Nav;
